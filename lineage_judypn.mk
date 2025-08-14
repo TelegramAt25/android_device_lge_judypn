322f16102a8b83a90f75a859c8cced581057ba8f
fukiame <fukiame@proton.me>
2025-08-15 00:36:04 +0700

    judypn: explicitly fry scudo
    from android go config
    
    fry ex early to not get screwed later on
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- lineage_judypn.mk
+++ lineage_judypn.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Malloc
+PRODUCT_DISABLE_SCUDO := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
